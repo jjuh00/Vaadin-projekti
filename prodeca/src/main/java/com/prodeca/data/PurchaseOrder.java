e3b11787912c60b321a2178e0796a19bc0412fd8
jjuh00 <jamsen.juho@gmail.com>
2026-03-29 22:04:32 +0300

    Deleted unused import

--- prodeca/src/main/java/com/prodeca/data/PurchaseOrder.java
+++ prodeca/src/main/java/com/prodeca/data/PurchaseOrder.java
@@ -7,7 +7,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Entiteetti 4: Tilaus (PurchaseOrder)
